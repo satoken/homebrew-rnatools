a825212e9255eed018442a340a44e41f432bf22e
Kengo Sato <satoken@bio.keio.ac.jp>
2016-02-07 17:00:41 +0900

    Version up: ractip

--- ractip.rb
+++ ractip.rb
@@ -1,9 +1,9 @@
 class Ractip < Formula
   desc "RNA-RNA interaction prediction by integer programming"
   homepage "https://github.com/satoken/ractip"
-  url "https://github.com/satoken/ractip/archive/v1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "0f128f34d7473d2d95bdecb182a62ca167edc253ab62e0f8c523c97082f37f79"
+  url "https://github.com/satoken/ractip/archive/v1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "cb9ef36655f0cc76a84e7c44bd0ae849a12ae6c9968e8dc767a518ed41e85b5b"
 
   depends_on "viennarna"
   depends_on "glpk"
